c2786da779b2b65ac49333b7b5fd932b637bf037
chantel <nyabokechantel83@gmail.com>
2026-08-01 20:58:05 +0300

    add the final.png

--- src/components/PosterLayout.jsx
+++ src/components/PosterLayout.jsx
@@ -1,11 +1,9 @@
 function PosterLayout({ children }) {
   return (
-    <div className="min-h-screen flex items-center justify-center bg-[#1a1a1a] p-8">
-      <div className="relative w-[792px] h-[1117px] bg-black overflow-hidden">
-        <div className="absolute top-0 left-[451px] w-[341px] h-[1117px] bg-white"></div>
+    <div className="relative w-[792px] h-[1117px] bg-[#000000] overflow-hidden">
+      <div className="absolute top-0 left-[451px] w-[341px] h-[1117px] bg-[#FFFFFF]" />
 
-        {children}
-      </div>
+      {children}
     </div>
   );
 }
